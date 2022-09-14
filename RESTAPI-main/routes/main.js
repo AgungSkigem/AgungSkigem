a4463df401176d256de30d4e109dcdda2fc8a01c
AgungSkigem <113549339+AgungSkigem@users.noreply.github.com>
2022-09-15 03:18:30 +0700

    Delete RESTAPI-main directory

--- RESTAPI-main/routes/main.js
+++
@@ -1,9 +0,0 @@
-__path = process.cwd()
-
-var express = require('express');
-var router = express.Router();
-
-router.get('/', (req, res) => {
-    res.sendFile(__path + '/views/index.html')
-})
-module.exports = router
